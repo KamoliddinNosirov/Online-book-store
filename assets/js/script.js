406c9a224dd6695ec79c0d8896e2d13c34d270d9
KamoliddinNosirov <kamoliddin3425@gmail.com>
2025-05-03 17:24:56 +0500

    Online book store

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var swiper = new Swiper(".heroSwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop:true
+  });
+  
+  var swiper2 = new Swiper(".heroSwiper2", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
+    loop:true
+  });
+
+  var swiper2 = new Swiper(".recommendedSwiper", {
+    spaceBetween: 20,
+    slidesPerView: 4,
+    centeredSlides: false,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+  });
