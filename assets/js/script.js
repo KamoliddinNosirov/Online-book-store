b797299c53450d4cb56390a50098eff7eca0f696
KamoliddinNosirov <kamoliddin3425@gmail.com>
2025-05-04 06:18:00 +0500

    html & css

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,36 @@ var swiper = new Swiper(".heroSwiper", {
     loop:true
   });
 
-  var swiper2 = new Swiper(".recommendedSwiper", {
+  var swiper3 = new Swiper(".recommendedSwiper", {
+    spaceBetween: 20,
+    slidesPerView: 4,
+    centeredSlides: false,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+  });
+
+  var swiper3 = new Swiper(".BooksOnSalesSwiper", {
     spaceBetween: 20,
     slidesPerView: 4,
     centeredSlides: false,
